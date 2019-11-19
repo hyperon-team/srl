9828ea746699d4d60c4e45dc1f388c9d0f3d0a7b
zargovv <42868763+maxzargov@users.noreply.github.com>
2019-11-19 09:17:31 +0600

    Added put (update) method

--- service/routes/servers.js
+++ service/routes/servers.js
@@ -31,8 +31,32 @@ router.post('/:invite', (req, res) => {
       console.log(body);
       // Invite validation
       // Error if invite is not valid
+      // Error if user is not the owner of the server
       // Get server info ( Icon (probably not needed), Name )
       // Add info to db
+      res.send({ ok: 1 });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  // Get server with id of 'id' from db
+  // Response with error if server not found Or there is an error
+  const { desc } = req.query;
+  // Check description if needed
+
+  // Getting guild invite
+  const { invite } = guild;
+  request
+    .get(`https://discordapp.com/api/v6/invites/${invite}`)
+    .then(body => JSON.parse(body))
+    .then(body => {
+      // Invite information
+      console.log(body);
+      // Invite validation
+      // Error if invite is not valid
+      // Error if user is not the owner of the server
+      // Update information about the server
       res.send({ ok: 1 });
     });
 });
