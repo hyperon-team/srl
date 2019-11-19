6488418168511e00f001fccf22733e3ce407cfcb
zargovv <42868763+maxzargov@users.noreply.github.com>
2019-11-19 08:24:15 +0600

    Update servers.js

--- service/routes/servers.js
+++ service/routes/servers.js
@@ -1,11 +1,40 @@
 import express from 'express';
+import request from '../lib/request';
 
 const router = express.Router();
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
+router.get('/', (req, res) => {
+  // Get servers from db
+  // Response with error if there is an error
+  // Return server list
+  res.send({ ok: 1 });
 });
 
-router.post('/:invite');
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  // Get server with id of 'id' from db
+  // Response with error if server not found Or there is an error
+  // Return server
+  res.send({ ok: 1 });
+});
+
+router.post('/:invite', (req, res) => {
+  // Invite should be invite CODE.
+  const { invite } = req.params;
+  const { desc } = req.query;
+  // Check description if needed
+  request
+    .get(`https://discordapp.com/api/v6/invites/${invite}`)
+    .then(body => JSON.parse(body))
+    .then(body => {
+      // Invite information
+      console.log(body);
+      // Invite validation
+      // Error if invite is not valid
+      // Get server info ( Icon (probably not needed), Name )
+      // Add info to db
+      res.send({ ok: 1 });
+    });
+});
 
 export default router;
